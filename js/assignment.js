664d1033cce98d04854a49e749c76b614dfe4885
Chris Lopez <Chris Lopez>
2017-04-12 23:30:10 -0700

    first commit

---
+++ js/assignment.js
@@ -0,0 +1,13 @@
+var firstName = prompt('What is your first name?');
+
+var lastName = prompt('What is your last name?');
+
+var age = prompt('What is your age?');
+
+var place = prompt('What is your location?');
+
+confirm('your location is ' + place);
+
+confirm('your name is ' + firstName + ' ' + lastName);
+
+console.log(age);
